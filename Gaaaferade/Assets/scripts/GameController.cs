336085b4027dc0e3f9a26806dd984905fe223a04
AnanoAspanidze <anano.aspanidze@gmail.com>
2016-03-20 21:55:08 +0400

    ჰოლა

---
+++ Gaaaferade/Assets/scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public List<GameObject> gzebi;
+
+    public GameObject ufero;
+    public GameObject feradi;
+    public GameObject button;
+   
+
+	void Start () {
+       
+	}
+	
+	
+	void Update () {
+	
+	}
+
+
+
+    public void onRoadMouseDown()
+    {
+        for (int i = 0; i < gzebi.Count; i++)
+        {
+            //var GameObjectPosition = gzebi[i].transform.position;
+            gzebi[i].GetComponent<SpriteRenderer>().sortingLayerName = "Default";
+       
+            //gzebi[i].transform.position = new Vector3(GameObjectPosition.x, GameObjectPosition.y, 0);
+        }      
+    }
+
+
+
+    public void changePicture()
+    {
+        Instantiate(feradi, ufero.transform.position, ufero.transform.rotation);
+        button.GetComponent<Button>().interactable = false;
+    }
+  
+}
---
+++ Gaaaferade/Assets/scripts/GzebisScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GzebisScript : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+
+
+
+
+    void OnMouseDown()
+    {
+        gameObject.GetComponent<GameController>().onRoadMouseDown();
+
+        gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "yvelazeCin";
+
+        //var GameObjectPosition = gameObject.transform.position;
+        //gameObject.transform.position = new Vector3(GameObjectPosition.x, GameObjectPosition.y, -1);   
+    }
+
+
+
+}
